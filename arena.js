24ee7f210966a064a6484c12c55aea9a460569cb
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-09-23 10:38:45 -0400

    first commit

---
+++ arena.js
@@ -0,0 +1,13 @@
+class Weapon {
+  constructor (name){
+    this.name = this validate
+  }
+}
+
+class Gladiator {
+  constructor (name, weapon){
+    this.name = name,
+    this.weapon = weapon,
+  }
+
+}
