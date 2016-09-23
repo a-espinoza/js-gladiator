53232e1dcdbd29df06e5b575c67221e4106a1e95
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-09-23 13:43:42 -0400

    3

--- arena.js
+++ arena.js
@@ -1,16 +1,40 @@
 class Arena{
-  constructor(name){
-    this.gladiator = []
-    this.name = capitalizeFirstLetter (name);
-  }
-  addGladiator (name){
-    this.gladiator.push(name);
-}
-}
+   constructor(name){
+     this.gladiators = [];
+     this.name = capitalizeFirstLetter(name);
+   }
+  addGladiator (name) {
+     if (this.gladiators.length < 2) {
+     this.gladiators.push(name);
+     }
+     else {
+     document.write("Gladiators are big, keep it less han 2 inside the arena!");
+     }
+   }
+   fight() {
+     var gladiator1 = this.gladiators[0].weapon;
+     var gladiator2 = this.gladiators[1].weapon;
+     if (gladiator1 === "trident" && gladiator2 === "spear"){
+       console.log("Gladiator 1 wins!");
+     }
+     else if (gladiator1 === "spear" && gladiator2 === "club"){
+       console.log("Gladatior 1 wins!")
+     }
+     else if (gladiator1 === "club" && gladiator2 === "trident" ) {
+       console.log("Gladiator 1 wins!")
+     }
+     else if (gladiator1 = gladiator2) {
+       console.log("Get outta here!!!")
+     }
+     else {
+       console.log("Gladiator 2 wins!")
+     }
+     }
+   }
 
-function capitalizeFirstLetter (inStr){
-  return inStr.replace(/\w\S*/g, function (name2){
-     return name2.charAt(0).toUpperCase() +name2.substr(1).toLowerCase();
+ function capitalizeFirstLetter(inStr) {
+   return inStr.replace(/\w\S*/g, function(name2)
+   { return name2.charAt(0).toUpperCase() + name2.substr(1).toLowerCase();
 
    });
-}
+ }
--- gladiator.js
+++ gladiator.js
@@ -1,10 +1,11 @@
-class Weapon {
-  constructor (name){
-    if(weapon === "spear" || wapon === "club" || weapon === "trident" ){
-    this.weapon = weapon;
-  }
-    else {
-      alert("Choose right")
+class Gladiator {
+   constructor(name, weapon){
+    this.name = name;
+     if (weapon === "spear" || weapon === "club" || weapon === "trident") {
+       this.weapon = weapon;
+     }
+     else {
+       alert("You didn't choose the right weapon for your Gladiator.");
     }
-  }
-}
+   }
+ }
